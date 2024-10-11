facaa7ad83f3a55c1031d551134837e37e5e81c8
caryang2004 <Ki.Yang@edu.sait.ca>
2024-10-11 10:31:08 -0600

    week-5 submission

---
+++ app/week-5/page.js
@@ -0,0 +1,12 @@
+import NewItem from "./new-item"
+
+export default function CounterPage(){
+
+    let count = 0;
+    
+    return(
+        <main>
+            <NewItem/>
+        </main>
+    );
+}
